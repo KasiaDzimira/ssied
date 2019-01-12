ac3b8de8de058c1736afb389edc1d9eb58c5a6b6
KasiaDzimira <katarzyna.dzimira@onet.pl>
2019-01-12 14:07:35 +0100

    creating array contains vectors for words

--- main.R
+++ main.R
@@ -10,11 +10,19 @@ test <- fread("test.csv", data.table = FALSE)
 
 #Tokenizing
 
-max_words <- 15000
-
 all_questions <- rbind(train %>% select(question_text), test %>% select(question_text))
 question_texts <- all_questions$question_text
-result <- tokenizers::tokenize_ngrams(question_texts, n = 1)
+result <- tokenizers::tokenize_words(question_texts)
+
+words = array(0);
+
+for (i in 1:length(result)){
+  for (word in result[[i]]) {
+    words[i] <- word
+  }
+}
+
+words = unique(words)
 
 embeddings <- readLines('wiki-news-300d-1M.vec')
 embeddings_index = new.env(hash = TRUE, parent = emptyenv())
@@ -22,7 +30,18 @@ embeddings <- embeddings[2:length(embeddings)]
 
 for (i in 1:length(embeddings)){
   embedding <- embeddings[[i]]
-  embedding <- strsplit(embedding, " ")[[1]]
-  word <- embedding[[1]]
-  embeddings_index[[word]] = as.double(embedding[-1])
+  values <- strsplit(embedding, " ")[[1]]
+  word <- values[[1]]
+  embeddings_index[[word]] = as.double(values[-1])
+}
+
+word_vectors = array(0, c(82500, 300))
+i = 1
+for (word in words) {
+  embedding_vector = embeddings_index[[word]]
+  
+  if (!is.null(embedding_vector)) {
+    i = i + 1
+    word_vectors[i,] <- embedding_vector
+  }
 }
